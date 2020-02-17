01b2b280a321867de93b4c517a7a65d21b993fa8
baloyar <baloyar@unisa.ac.za>
2020-02-17 17:43:48 +0200

    Changed the the way study levels are retrieved, we now get them from qspsun and not no longer from sun

--- sakai-11.4/unisa-tools/unisa-tpustudentplacement/src/java/za/ac/unisa/lms/tools/tpustudentplacement/forms/StudentImplHelper.java
+++ sakai-11.4/unisa-tools/unisa-tpustudentplacement/src/java/za/ac/unisa/lms/tools/tpustudentplacement/forms/StudentImplHelper.java
@@ -7,25 +7,5 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class StudentImplHelper {
-	   
-	
-	  public  String  getStudentData(Student student,short acadYear,short semester)throws Exception{
-             		  //this methods assumes that student.getNumber() gives a valid student number 
-                            StudentValidator studValidator=new StudentValidator ();  
-                            String validatorRes="";
-                            int stuNum=student.getNumber();
-                            student.getStudent(stuNum);
-                            Qualification qual=student.getStudentQual(stuNum, acadYear);
-                            validatorRes=studValidator.validateQualification(qual);
-                            List moduleList=new ArrayList();
-                            if(validatorRes.equals("")){
-                                     moduleList=student.getStudentPracticalModuleList(stuNum,acadYear,semester);
-                        	         validatorRes=studValidator.validatePracticalModuleList(moduleList);
-                            }
-                        	if(validatorRes.equals("")){
-                        		     student.setStudentData(student,stuNum,acadYear,semester);
-                        	}
-                       return validatorRes;
-         }
-
+		 
 }
